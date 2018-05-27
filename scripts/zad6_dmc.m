00b26d5f0a851f7ecf44e2627cae652dd5a3b841
kuba <J.Sikora@stud.elka.pw.edu.pl>
2018-05-28 00:16:00 +0200

    koniec z kodami

---
+++ scripts/zad6_dmc.m
@@ -0,0 +1,92 @@
+T_mn = 1;
+K_mn = 0.45587;
+
+
+%Dane regulatora
+kk = 2000;
+y_zad = zeros(1,kk);
+y_zad(13:kk) = 1;
+du = zeros(1,kk);
+N = 20;
+Nu = 1;
+lambda = 1;
+D = 80;
+
+
+%Wyznaczenie odpowiedzi skokowej
+K0 = K_mn*4.3;
+T0 = T_mn*5;
+T1 = 1.85;
+T2 = 5.35;
+
+Tp = 0.5;
+s = tf('s');
+Gs = (K0 * exp(-s*T0))/((T1*s + 1)*(T2*s +1));
+Gz = c2d(Gs, Tp, 'zoh');
+
+odp_skok = step(Gz, 0:Tp:2*D);
+
+y_zad = zeros(1,kk);
+y_zad(D:kk) = 1;
+y=zeros(1,kk);
+u=zeros(1,kk);
+
+% Wyznaczenie macierzy dynamicznej
+M = zeros(N, Nu);
+for i=1:Nu
+    for j=1:N
+        if j >= i
+            M(j+i-1,i) = odp_skok(j);
+        else
+            M(j,i) = 0;
+        end
+    end
+end
+M = M(1:N, 1:Nu);
+
+% Wyznaczenie macierzy predykcji
+Mp = zeros(N,D-1);
+for j=1:(D-1)
+    for i=1:N
+        Mp(i,j) = odp_skok(i+j) - odp_skok(j);
+    end
+end
+
+% Wyznaczenie wektora K
+
+K = (M'*M + lambda*eye(Nu))^-1 * M';
+
+K1 = K(1,1:N);
+ke = sum(K1);
+
+
+n = cell2mat(Gz.num);
+d = cell2mat(Gz.den);
+
+for k=D:kk
+    y(k) = 1.674*y(k-1) - 0.6951*y(k-2) + 0.04818*u(k - 1 - 10*T_mn) + 0.04268*u(k - 2 - 10*T_mn);
+    swob = 0;
+    for j=1:(D-1)
+        ku = K1*Mp(:,j);
+        swob = swob + ku*du(k-j);
+    end
+    du(k) = ke*(y_zad(k) - y(k)) - swob;
+    u(k) = u(k-1) + du(k);
+end
+
+
+fig1 = figure;
+t = linspace(1,kk,kk);
+hold on
+plot(t, y_zad, 'g');
+stairs(t, y, 'r');
+grid on
+grid minor
+legend('y_{zad}[k]', 'y[k]');
+xlabel('Próbki dyskretne k');
+ylabel('Wartoœæ wyjœcia y');
+title('Badanie obszarów stabilnoœci regulatora DMC');
+hold off
+
+name = strcat('figures\dmc6_Tmn_', strrep(num2str(T_mn), '.', ','));
+print_figure(name, '..\figures')
---
+++ scripts/zad6_fig.m
@@ -0,0 +1,28 @@
+hold on
+xlim([1.0 2.0]);
+fig1 = area(data6_dmc(1,:), data6_dmc(2,:));
+fig1.FaceColor = [0 0 1];
+xlabel('T_0/T_0^{nom}')
+ylabel('K_0/K_0^{nom}')
+title('Wyznaczone obszary stabilnoœci regulatora DMC')
+hold off
+
+
+X = zeros(11, 2);
+X(:,1) = data_zad6(1,:);
+X(:,2) = data_zad6(1,:)';
+Y = zeros(11, 2);
+Y(:,1) = data_zad6(2,:)';
+Y(:,2) = 5*ones(11,1) - data_zad6(2,:)';
+
+
+
+figure
+xlim([1.0 2.0]);
+    h =area(X,Y);
+    h(2).FaceColor = [0 0 1];
+    h(1).Visible = 'off';
+xlabel('T_0/T_0^{nom}')
+ylabel('K_0/K_0^{nom}')
+title('Wyznaczone obszary stabilnoœci regulatora PID')
+hold off
